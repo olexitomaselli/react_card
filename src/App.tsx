c286870ce87d5cc9504a0423c2bcb25ae90fa335
Ol3xiii <olexitom29@gmail.com>
2023-02-23 17:53:23 +0100

    Added images, and position centre cards

--- src/App.tsx
+++ src/App.tsx
@@ -6,33 +6,39 @@ export type Product = {
   model: string;
   description: string;
   price: number;
+  image: string;
 };
 
 const products: Product[] = [  
   {
     model: "Iphone", 
     description: "Top smartphone", 
-    price: 20
+    price: 20,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSgOoLh_HeAozlsEHKfUaMlicA6iNG71_NxzQ&usqp=CAU"
   },
   {
     model: "Samsung",
     description: "Nice smartphone but iphpone is more good", 
-    price: 10
+    price: 10,
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE4EbsYIVNW4JAXI2VH7ckxjDqHGAe1_3WjuaICHidCmay-hCJNTvO0Unt6KLbcVpNXiU&usqp=CAU"
   },
   {
     model: "huawei", 
     description: "nice phone but not good",
     price: 0.5,
+    image: "https://d1eh9yux7w8iql.cloudfront.net/product_images/1585915495.356295.jpg"
   }
 ];
 
 function App() {
   return (
-    <div>
-      {products.map((product) => (
-        <Card product={product} />
-      ))}
-    </div>
+    <body>
+      <div>
+        {products.map((product) => (
+          <Card product={product} />
+        ))}
+      </div>
+    </body>
   );
 }
 
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./Card.css";
 import { Product } from "../../App";
 
+//...snip
 //CardProps è un tipo che accetta come chiave product di tipo Product
 // n: Number è un esempio di un altro tipo che accetta CardProps
 
@@ -12,6 +13,7 @@ export const Card = (props: CardProps) => {
         <div className="card">
             <div className="card-body">
                 <h5 className="card-title">{props.product.model}</h5>
+                <img className="image" src={props.product.image} alt="" />
                 <p className="card-text">{props.product.description}</p>
                 <p className="card-text">{props.product.price}</p>
             </div>
